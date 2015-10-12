498bb39dfb2d8e6caaa13d33e98caa1772a9d221
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-10-12 17:33:13 +0200

    Remove unnecessary repeated escaping

--- includes/events/event-builder.php
+++ includes/events/event-builder.php
@@ -264,7 +264,7 @@ class Event_Builder {
 
 				case 'calendar' :
 				case 'feed-title' :
-					return esc_attr( $event->source );
+					return $event->source;
 
 				case 'id' :
 				case 'event-id' :
@@ -725,7 +725,7 @@ class Event_Builder {
 		$anchor = $tag != 'url' ? 'yes' : $attr['autolink'];
 		$target = $attr['newwindow'] !== false ? 'target="_blank"' : '';
 
-		return $anchor !== false ? ' <a href="' . esc_url( $url ) . '" ' . $target . '>' . $text . '</a>' : ' ' . $text;
+		return $anchor !== false ? ' <a href="' . $url . '" ' . $target . '>' . $text . '</a>' : ' ' . $text;
 	}
 
 	/**
