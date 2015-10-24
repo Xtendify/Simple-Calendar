033adba41ffc64f6baca2aedbd865ca2f314072c
Phil Derksen <pderksen@gmail.com>
2015-10-24 15:09:27 -0700

    Welcome page: Add Add-ons link button

--- includes/admin/welcome.php
+++ includes/admin/welcome.php
@@ -166,6 +166,9 @@ class Welcome {
 			<a href="<?php echo admin_url( 'admin.php?page=simple-calendar_settings' ); ?>"
 			   class="button button-primary"
 				><?php _e( 'Settings', 'google-calendar-events' ); ?></a>
+			<a href="<?php echo simcal_ga_campaign_url( \SimpleCalendar\plugin()->get_url( 'add-ons' ), 'core-plugin', 'welcome-page' ); ?>"
+			   class="docs button button-primary" target="_blank"
+				><?php _e( 'Add-ons', 'google-calendar-events' ); ?></a>
 			<a href="<?php echo simcal_ga_campaign_url( \SimpleCalendar\plugin()->get_url( 'docs' ), 'core-plugin', 'welcome-page' ); ?>"
 			   class="docs button button-primary" target="_blank"
 				><?php _e( 'Docs', 'google-calendar-events' ); ?></a>
