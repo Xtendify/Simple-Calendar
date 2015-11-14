4470c89726b709d3e1cb272a0bf3fda48cd3385e
Phil Derksen <pderksen@gmail.com>
2015-11-14 15:24:28 -0800

    Change default today color to WP color picker blue (#1e73be)

--- includes/calendars/admin/default-calendar-admin.php
+++ includes/calendars/admin/default-calendar-admin.php
@@ -288,7 +288,7 @@ class Default_Calendar_Admin {
 
 			$settings                   = get_option( 'simple-calendar_settings_calendars' );
 			$default_theme              = isset( $settings['default-calendar']['theme'] ) ? $settings['default-calendar']['theme'] : 'light';
-			$default_today_color        = /*isset( $settings['default-calendar']['today_color'] ) ? $settings['default-calendar']['today_color'] :*/ '#FF0000';
+			$default_today_color        = /*isset( $settings['default-calendar']['today_color'] ) ? $settings['default-calendar']['today_color'] :*/ '#1e73be';
 			$default_days_events_color  = /*isset( $settings['default-calendar']['days_events_color'] ) ? $settings['default-calendar']['days_events_color'] :*/ '#000000';
 
 			?>
