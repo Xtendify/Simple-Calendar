c0f320e62b818ce54dc8a3d3e41765a97c753e65
Phil Derksen <pderksen@gmail.com>
2015-12-17 10:52:31 -0800

    Grunt: Update checktextdomain keywords based on standard at https://github.com/stephenharris/grunt-checktextdomain

--- gruntfile.js
+++ gruntfile.js
@@ -177,9 +177,8 @@ module.exports = function( grunt ) {
 
 		checktextdomain: {
 			options: {
-				text_domain   : 'google-calendar-events',
-				correct_domain: false,
-				keywords      : [
+				text_domain: 'google-calendar-events',
+				keywords: [
 					'__:1,2d',
 					'_e:1,2d',
 					'_x:1,2c,3d',
@@ -193,15 +192,11 @@ module.exports = function( grunt ) {
 					'_n:1,2,4d',
 					'_nx:1,2,4c,5d',
 					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d',
-					' __ngettext:1,2,3d',
-					'__ngettext_noop:1,2,3d',
-					'_c:1,2d',
-					'_nc:1,2,4c,5d'
+					'_nx_noop:1,2,3c,4d'
 				]
 			},
-			files  : {
-				src   : [
+			files: {
+				src: [
 					'includes/**/*.php',
 					'google-calendar-events.php',
 					'uninstall.php'
