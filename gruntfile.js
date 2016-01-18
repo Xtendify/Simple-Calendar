539d47fc4f1b95450e6915c4e07b0941edb95142
Phil Derksen <pderksen@gmail.com>
2016-01-18 11:09:48 -0800

    Grunt: Alphabetize tasks in gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -22,9 +22,9 @@ module.exports = function( grunt ) {
 		'!composer.json',
 		'!composer.lock',
 		'!contributing.md',
-		'!readme.md',
 		'!gruntfile.js',
 		'!package.json',
+		'!readme.md',
 		'!**/*~'
 	];
 
@@ -38,6 +38,87 @@ module.exports = function( grunt ) {
 		        ' * Licensed GPLv2+' +
 		        ' */\n',
 
+		checktextdomain: {
+			options: {
+				text_domain: 'google-calendar-events',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src: [
+					'includes/**/*.php',
+					'google-calendar-events.php',
+					'uninstall.php'
+				],
+				expand: true
+			}
+		},
+
+		clean: {
+			build: [ 'build' ]
+		},
+
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: './build/simple-calendar-<%= pkg.version %>.zip'
+				},
+				expand: true,
+				src: distFiles,
+				dest: '/google-calendar-events'
+			}
+		},
+
+		// TODO Add copy tasks for JS & CSS from bower_components/
+		// Include .map files from qTip2, etc.
+		// qtip2.js vs jquery.qtip2.js file naming?
+		copy: {
+			main: {
+				expand: true,
+				src: distFiles,
+				dest: 'build/google-calendar-events'
+			}
+		},
+
+		cssmin: {
+			options: {
+				processImport: false,
+				keepSpecialComments: 0
+			},
+			minify: {
+				expand: true,
+				cwd: 'assets/css',
+				src: [
+					'*.css',
+					'!*.min.css'
+				],
+				dest: 'assets/css',
+				ext: '.min.css'
+			}
+		},
+
+		jscs: {
+			all: [
+				'assets/js/*.js',
+				'!assets/js/*.min.js'
+			]
+		},
+
 		jshint: {
 			options: {
 				ignores: [
@@ -50,27 +131,20 @@ module.exports = function( grunt ) {
 			]
 		},
 
-		jscs: {
-			all: [
-				'assets/js/*.js',
-				'!assets/js/*.min.js'
-			]
-		},
-
-		uglify: {
-			all: {
-				files: {
-					'assets/js/admin.min.js': [ 'assets/js/admin.js' ],
-					'assets/js/admin-add-calendar.min.js': [ 'assets/js/admin-add-calendar.js' ],
-					'assets/js/default-calendar.min.js': [ 'assets/js/default-calendar.js' ]
-				},
-				options: {
-					mangle: {
-						except: [ 'jQuery' ]
-					},
-					sourceMap: false,
-					preserveComments: false
-				}
+		postcss: {
+			options: {
+				processors: [
+					require( 'autoprefixer' )( { browsers: 'last 2 versions' } )
+				]
+			},
+			dist: {
+				expand: true,
+				cwd: 'assets/css',
+				src: [
+					'*.css',
+					'!*.min.css'
+				],
+				dest: 'assets/css'
 			}
 		},
 
@@ -92,37 +166,20 @@ module.exports = function( grunt ) {
 			}
 		},
 
-		postcss: {
-			options: {
-				processors: [
-					require( 'autoprefixer' )( { browsers: 'last 2 versions' } )
-				]
-			},
-			dist: {
-				expand: true,
-				cwd: 'assets/css',
-				src: [
-					'*.css',
-					'!*.min.css'
-				],
-				dest: 'assets/css'
-			}
-		},
-
-		cssmin: {
-			options: {
-				processImport: false,
-				keepSpecialComments: 0
-			},
-			minify: {
-				expand: true,
-				cwd: 'assets/css',
-				src: [
-					'*.css',
-					'!*.min.css'
-				],
-				dest: 'assets/css',
-				ext: '.min.css'
+		uglify: {
+			all: {
+				files: {
+					'assets/js/admin.min.js': [ 'assets/js/admin.js' ],
+					'assets/js/admin-add-calendar.min.js': [ 'assets/js/admin-add-calendar.js' ],
+					'assets/js/default-calendar.min.js': [ 'assets/js/default-calendar.js' ]
+				},
+				options: {
+					mangle: {
+						except: [ 'jQuery' ]
+					},
+					sourceMap: false,
+					preserveComments: false
+				}
 			}
 		},
 
@@ -173,64 +230,8 @@ module.exports = function( grunt ) {
 					debounceDelay: 500
 				}
 			}
-		},
-
-		checktextdomain: {
-			options: {
-				text_domain: 'google-calendar-events',
-				keywords: [
-					'__:1,2d',
-					'_e:1,2d',
-					'_x:1,2c,3d',
-					'esc_html__:1,2d',
-					'esc_html_e:1,2d',
-					'esc_html_x:1,2c,3d',
-					'esc_attr__:1,2d',
-					'esc_attr_e:1,2d',
-					'esc_attr_x:1,2c,3d',
-					'_ex:1,2c,3d',
-					'_n:1,2,4d',
-					'_nx:1,2,4c,5d',
-					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d'
-				]
-			},
-			files: {
-				src: [
-					'includes/**/*.php',
-					'google-calendar-events.php',
-					'uninstall.php'
-				],
-				expand: true
-			}
-		},
-
-		clean: {
-			build: [ 'build' ]
-		},
-
-		// TODO Add copy tasks for JS & CSS from bower_components/
-		// Include .map files from qTip2, etc.
-		// qtip2.js vs jquery.qtip2.js file naming?
-		copy: {
-			main: {
-				expand: true,
-				src: distFiles,
-				dest: 'build/google-calendar-events'
-			}
-		},
-
-		compress: {
-			main: {
-				options: {
-					mode: 'zip',
-					archive: './build/simple-calendar-<%= pkg.version %>.zip'
-				},
-				expand: true,
-				src: distFiles,
-				dest: '/google-calendar-events'
-			}
 		}
+
 	} );
 
 	require( 'load-grunt-tasks' )( grunt );
