651844a6cb09c92af17b6e07a8374a010a3bbbb0
Phil Derksen <pderksen@gmail.com>
2016-09-22 14:32:36 -0700

    Set event object end properties in same order as begin properties.

--- includes/events/event.php
+++ includes/events/event.php
@@ -318,13 +318,13 @@ class Event {
 
 		if ( ! empty( $event['end'] ) ) {
 			$this->end = is_numeric( $event['end'] ) ? intval( $event['end'] ): false;
+			if ( ! empty( $event['end_utc'] ) ) {
+				$this->end_utc = is_numeric( $event['end_utc'] ) ? intval( $event['end_utc'] ) : false;
+			}
 			if ( ! empty( $event['end_timezone'] ) ) {
 				$this->end_timezone = esc_attr( $event['end_timezone'] );
 			}
-			if ( ! empty( $event['end_utc'] ) ) {
-				$this->end_utc = is_numeric( $event['end_utc'] ) ? intval( $event['end_utc'] ) : false;
-				$this->end_dt = Carbon::createFromTimestamp( $this->end, $this->end_timezone );
-			}
+			$this->end_dt = Carbon::createFromTimestamp( $this->end, $this->end_timezone );
 			$end_location = isset( $event['end_location'] ) ? $event['end_location'] : '';
 			$this->end_location = $this->esc_location( $end_location );
 		}
