e4dd398d8390b5f342b385fb36df6d4c8466d0fd
Phil Derksen <pderksen@gmail.com>
2017-03-29 14:20:36 -0700

    Update copyright & GPL notice in main plugin file header.

--- google-calendar-events.php
+++ google-calendar-events.php
@@ -8,8 +8,24 @@
  * Version:     3.1.9
  * Text Domain: google-calendar-events
  * Domain Path: /i18n
+ */
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * @copyright   2015-2016 Moonstone Media/Phil Derksen. All rights reserved.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright 2015-2017 Moonstone Media Group. All rights reserved.
  */
 
 // Exit if accessed directly.
