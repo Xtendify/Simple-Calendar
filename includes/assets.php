ab16dfd32643c45af0e78233b18b64b5bd303f12
Cesar Rodriguez <crod386@gmail.com>
2019-06-26 22:45:08 -0700

    Add jquery as dependency for all enqueued scripts

--- includes/assets.php
+++ includes/assets.php
@@ -219,7 +219,7 @@ class Assets {
 					$src        = esc_url( $v['src'] );
 					$in_footer  = isset( $v['in_footer'] )   ? $v['in_footer']  : false;
 
-					wp_enqueue_script( $script, $src, array(), SIMPLE_CALENDAR_VERSION, $in_footer );
+					wp_enqueue_script( $script, $src, array( 'jquery' ), SIMPLE_CALENDAR_VERSION, $in_footer );
 
 					if ( ! empty( $v['localize'] ) && is_array( $v['localize'] ) ) {
 						foreach ( $v['localize'] as $object => $l10n ) {
