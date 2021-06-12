397a4dcabeb6f955f54172e6f8c2c1cd35dc763d
mickwall001 <70205602+mickwall001@users.noreply.github.com>
2021-06-12 18:38:34 +0100

    add erusev to php scoper

--- scoper.inc.php
+++ scoper.inc.php
@@ -73,6 +73,7 @@ return array(
 			->path( '#^nesbot/#' )
 			->path( '#^symfony/#' )
 			->path( '#^mexitek/#' )
+			->path( '#^erusev/#' )
 			->in( 'vendor' ),
 
 		// Google API service infrastructure classes.
