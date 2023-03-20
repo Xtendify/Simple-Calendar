0b580bc17a8b81cfbb94dc852b0efaba1521acc1
Rohit Singhal <rohitsinghal.rs@gmail.com>
2023-03-20 21:26:01 +0530

    remove polyfills since PHP scoper has it by default

--- scoper.inc.php
+++ scoper.inc.php
@@ -27,28 +27,6 @@ $google_services = implode(
 	)
 );
 
-$polyfillsBootstraps = array_map(
-	static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-	iterator_to_array(
-		Finder::create()
-			->files()
-			->in(__DIR__ . '/vendor/symfony/polyfill-*')
-			->name('bootstrap*.php'),
-		false,
-	),
-);
-
-$polyfillsStubs = array_map(
-	static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-	iterator_to_array(
-		Finder::create()
-			->files()
-			->in(__DIR__ . '/vendor/symfony/polyfill-*/Resources/stubs')
-			->name('*.php'),
-		false,
-	),
-);
-
 return array(
 	'prefix' 					 => 'SimpleCalendar\plugin_deps',
 	'finders'                    => array(
@@ -106,19 +84,9 @@ return array(
 			->name("#($google_services)\.php#")
 			->in('vendor/google/apiclient-services/src'),
 	),
-	'exclude-namespaces' => [
-        'Symfony\Polyfill'
-    ],
-	'exclude-constants' => [
-        // Symfony global constants
-        '/^SYMFONY\_[\p{L}_]+$/',
-    ],
 	'exclude-files'            => array(
-
 		// This dependency is a global function which should remain global.
 		'vendor\\ralouphie\\getallheaders\\src\\getallheaders.php',
-        ...$polyfillsBootstraps,
-        ...$polyfillsStubs,
 	),
 	'exclude-classes' => [
         'Isolated\Symfony\Component\Finder\Finder',
