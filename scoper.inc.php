69e859c75a0d63e5257044d79c0f2a55d77c7c3c
Rohit Singhal <rohitsinghal.rs@gmail.com>
2023-08-21 19:46:26 +0530

    Add polyfill (#487)
    * Add polyfill
    
    * lint:fix
    
    * Update version and changelog

--- scoper.inc.php
+++ scoper.inc.php
@@ -24,6 +24,28 @@ $google_services = implode(
 	)
 );
 
+$polyfillsBootstraps = array_map(
+	static fn(SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+	iterator_to_array(
+		Finder::create()
+			->files()
+			->in(__DIR__ . '/vendor/symfony/polyfill-*')
+			->name('bootstrap*.php'),
+		false
+	)
+);
+
+$polyfillsStubs = array_map(
+	static fn(SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+	iterator_to_array(
+		Finder::create()
+			->files()
+			->in(__DIR__ . '/vendor/symfony/polyfill-*/Resources/stubs')
+			->name('*.php'),
+		false
+	)
+);
+
 return [
 	'prefix' => 'SimpleCalendar\plugin_deps',
 	'finders' => [
@@ -66,6 +88,13 @@ return [
 	'exclude-files' => [
 		// This dependency is a global function which should remain global.
 		'vendor\\ralouphie\\getallheaders\\src\\getallheaders.php',
+		...$polyfillsBootstraps,
+		...$polyfillsStubs,
+	],
+	'exclude-namespaces' => ['Symfony\Polyfill'],
+	'exclude-constants' => [
+		// Symfony global constants
+		'/^SYMFONY\_[\p{L}_]+$/',
 	],
 	'exclude-classes' => ['Isolated\Symfony\Component\Finder\Finder'],
 ];
