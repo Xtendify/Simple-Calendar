56c29f73275d06e93507be94025509b6b7060606
Akhill2020 <122770578+Akhill2020@users.noreply.github.com>
2024-09-24 12:17:03 +0530

    index issue (#574)

--- includes/calendars/views/default-calendar-grid.php
+++ includes/calendars/views/default-calendar-grid.php
@@ -288,7 +288,7 @@ class Default_Calendar_Grid implements Calendar_View
 
 			<?php
    $is_print_calendar = get_post_meta($calendar->id, '_display_print_calendar');
-   if ($is_print_calendar[0] === 'yes') {
+   if (isset($is_print_calendar[0]) && !empty($is_print_calendar[0]) && $is_print_calendar[0] === 'yes') {
    	echo '<button id="print-calendar-button" class="print-calendar-button demo_sc_primary_btn">Print Calendar</button>';
    }
    }
