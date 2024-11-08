4eb7e8825ca86d3f4bb02614daac2cb780161889
Rohit Singhal <rohitsinghal.rs@gmail.com>
2024-11-08 12:38:56 +0530

    Allow using SCSS

--- .bin/esbuild.mjs
+++ .bin/esbuild.mjs
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
 import pkg from '../package.json' assert { type: 'json' };
 
 const watch = process.argv.includes('--watch');
@@ -20,7 +21,7 @@ const files = [
 	{ in: 'assets/js/oauth-helper-admin.js', out: 'oauth-helper-admin.min' },
 	{ in: 'assets/css/admin-add-calendar.css', out: 'admin-add-calendar.min' },
 	{ in: 'assets/css/oauth-helper-admin.css', out: 'oauth-helper-admin.min' },
-	{ in: 'assets/css/admin-post-setting-style.css', out: 'admin-post-setting-style.min' },
+	{ in: 'assets/css/admin-post-setting-style.scss', out: 'admin-post-setting-style.min' },
 	{ in: 'assets/css/admin-sett-style.css', out: 'admin-sett-style.min' },
 	{ in: 'assets/css/admin.css', out: 'admin.min' },
 	{ in: 'assets/css/default-calendar-grid.css', out: 'default-calendar-grid.min' },
@@ -50,6 +51,7 @@ const config = {
 		'.svg': 'file',
 	},
 	outdir: 'assets/generated',
+	plugins: [sassPlugin()]
 };
 
 if (watch) {
